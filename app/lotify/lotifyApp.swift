2b54219326443c26269041c31a554105d1a442a9
leanderkretschmer <leanderkretschmer@icloud.com>
2025-07-28 14:05:18 +0200

    added macos app

---
+++ app/lotify/lotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  lotifyApp.swift
+//  lotify
+//
+//  Created by Leander Kretschmer on 28.07.25.
+//
+
+import SwiftUI
+
+@main
+struct lotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
