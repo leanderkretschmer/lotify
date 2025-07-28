db37e492956d34ebd91aad03ddf97e0048a5b7cf
leanderkretschmer <leanderkretschmer@icloud.com>
2025-07-28 12:39:20 +0200

    Initial Commit

---
+++ lotifyTests/lotifyTests.swift
@@ -0,0 +1,16 @@
+//
+//  lotifyTests.swift
+//  lotifyTests
+//
+//  Created by Leander Kretschmer on 28.07.25.
+//
+
+import Testing
+
+struct lotifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
